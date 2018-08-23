e4a9fa08a13828eee55239e6968c12dd3cab0143
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2018-08-23 13:33:49 +0100

    Added comments any timing for problem 1

--- 1_multiples_of_3_and_5.py
+++ 1_multiples_of_3_and_5.py
@@ -1,7 +1,16 @@
+# Finds the sum of multiples of 3 and 5 below 1000
+
+import time
+
+start = time.clock()
+
 total = 0
 
 for i in range(1000):
     if i%3==0 or i%5==0:
         total+=i
 
+end = time.clock()
+
 print total
+print "Time taken: ", end-start, " s"
