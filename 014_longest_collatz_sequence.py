2778825cd6e1e490dc5cd6a506a58a6bfaf9d528
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2018-08-30 22:30:05 +0100

    Added solution for problem 14

---
+++ 014_longest_collatz_sequence.py
@@ -0,0 +1,34 @@
+# Finds the number under one million that generates the longest Collatz
+# sequence (en.wikipedia.org/wiki/Collatz_conjecture)
+# Another slow one. Possibly improve using memoisation?
+
+import time
+
+def update(num):
+    if num%2 == 0:
+        return num/2
+    return 3*num + 1
+
+def collatz_sequence_length(num):
+    length = 1
+    while num != 1:
+        num = update(num)
+        length += 1
+    return length
+
+start = time.clock()
+
+max_length = 0
+max_n = 0
+upper_bound = 1000000
+
+for n in range(1,upper_bound+1):
+    length = collatz_sequence_length(n)
+    if length > max_length:
+        max_length = length
+        max_n = n
+
+end = time.clock()
+
+print max_n
+print "Time taken: ", end-start, " s"
