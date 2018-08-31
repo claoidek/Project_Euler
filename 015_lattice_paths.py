64e0f0cdc8aa749beaa8df8514a850d7cbb8943f
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2018-08-31 22:33:06 +0100

    Added solution for problem 15

---
+++ 015_lattice_paths.py
@@ -0,0 +1,19 @@
+# Calculates the number of possible routes through a 20x20 grid when only down
+# and right moves are allowed.
+# This is equivalent to 40 choose 20, so we can use nCr
+# (en.wikipedia.org/wiki/Combination)
+
+import time
+import math
+
+start = time.clock()
+
+x = 20
+y = 20
+
+nCr = math.factorial(x+y)/(math.factorial(x)*math.factorial(y))
+
+end = time.clock()
+
+print nCr
+print "Time taken: ", end-start, " s"
