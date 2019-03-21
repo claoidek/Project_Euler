568284df93bc6765443e445ca07f9ff747579eab
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-03-21 15:11:08 +0000

    Added solution for problem 24

---
+++ 024_lexicographic_permutations.py
@@ -0,0 +1,20 @@
+# Finds the millionth lexicographic permutation of the digits 0123456789
+
+import time
+import itertools
+
+start = time.clock()
+
+digits = [0,1,2,3,4,5,6,7,8,9]
+
+permutation_count = 0
+
+for permutation in itertools.permutations(digits,10):
+    permutation_count += 1
+    if permutation_count == 1000000:
+        break
+
+end = time.clock()
+
+print "".join(str(x) for x in permutation)
+print "Time taken: ", end-start, " s"
