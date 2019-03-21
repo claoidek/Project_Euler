7d52e784da6d76f14a09d3c26394291fd98a3f9b
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-03-21 16:54:40 +0000

    Added solution for problem 28

---
+++ 028_number_spiral_diagonals.py
@@ -0,0 +1,28 @@
+# Finds the sum of all numbers along both diagonals in a 1001x1001 spiral
+# centred at 1.
+#
+# Example of a 5x5 spiral:
+# 21 22 23 24 25
+# 20  7  8  9 10
+# 19  6  1  2 11
+# 18  5  4  3 12
+# 17 16 15 14 13
+
+import time
+
+start = time.clock()
+
+spiral_width = 1001
+current_width = 3
+current_number = 1
+total = 1
+
+while current_width <= spiral_width:
+    total += current_number*4 + current_width*10 - 10
+    current_number += current_width*4 - 4
+    current_width += 2
+
+end = time.clock()
+
+print total
+print "Time taken: ", end-start, " s"
