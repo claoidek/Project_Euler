6509710cdbefbabd6d6b8a27b85cd6f0bb668646
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-03-22 12:10:31 +0000

    Added solution for problem 29

---
+++ 029_distinct_powers.py
@@ -0,0 +1,17 @@
+# Finds the number of distinct terms that can be represented in the form a^b
+# where 2 <= a,b <= 100
+
+import time
+
+start = time.clock()
+
+distinct_terms = set()
+
+for a in range(2,101):
+    for b in range(2,101):
+        distinct_terms.add(a**b)
+
+end = time.clock()
+
+print len(distinct_terms)
+print "Time taken: ", end-start, " s"
