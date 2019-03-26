2ac3fe206d61b9f8c9191b21b3969b72d302a4eb
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-03-26 14:35:14 +0000

    Added solution for problem 46

---
+++ 046_goldbachs_other_conjecture.py
@@ -0,0 +1,38 @@
+# Finds the first odd composite number that cannot be written as the sum of a
+# prime and twice a square.
+
+import time
+
+def is_prime(num):
+    for prime in primes:
+        if prime*prime>num:
+            primes.append(num)
+            return True
+        if num%prime==0:
+            return False
+    return True
+
+start = time.clock()
+
+primes = [2,3,5,7]
+
+num = 7
+found = False
+
+while not found:
+    num += 2
+    if is_prime(num):
+        continue
+    base = 1
+    while 2*base*base < num:
+        if num - 2*base*base in primes:
+            break
+        base += 1
+    if 2*base*base > num:
+        found = True
+
+
+end = time.clock()
+
+print num
+print "Time taken: ", end-start, " s"
