028c54003253d92742ccdb7bae86de22133eeecd
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-03-27 16:39:36 +0000

    Added solution for problem 52

---
+++ 052_permuted_multiples.py
@@ -0,0 +1,23 @@
+# Finds the smallest number x such that x,2x,3x,...,6x all contain the same
+# digits in different orders.
+
+import time
+
+start = time.clock()
+
+found = False
+
+num = 1
+
+while not found:
+    found = True
+    for i in range(2,7):
+        if sorted(str(num)) != sorted(str(num*i)):
+            found = False
+            break
+    num += 1
+
+end = time.clock()
+
+print num - 1
+print "Time taken: ", end-start, " s"
