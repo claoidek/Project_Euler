9ba966e8666d20630a2b1f3a3961b8e7e767703b
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-03-28 10:35:37 +0000

    Added solution for problem 55

---
+++ 055_lychrel_numbers.py
@@ -0,0 +1,35 @@
+# Finds all Lychrel numbers under 10000
+# A Lychrel number is a number that never produces a palindrome when iteratively
+# adding its reverse to itself
+# For example, 349 is not a Lychrel number because:
+# 349 + 943 = 1292
+# 1292 + 2921 = 4213
+# 4213 + 3124 = 7337, which is a palindrome
+
+import time
+
+def is_palindrome(num):
+    if str(num)==str(num)[::-1]:
+        return True
+    return False
+
+start = time.clock()
+
+total = 9999
+
+for i in range(1,10000):
+    iters = 1
+    num = i
+    # We assume a number is a Lychrel number if it takes more than 50 iterations
+    # to reach a palindrome
+    while iters < 50:
+        iters += 1
+        num += int(str(num)[::-1])
+        if is_palindrome(num):
+            total -= 1
+            break
+
+end = time.clock()
+
+print total
+print "Time taken: ", end-start, " s"
