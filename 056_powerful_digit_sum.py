b0495091e2a2291565bcdd4573614cbfd5b3f279
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-03-28 10:40:44 +0000

    Added solution for problem 56

---
+++ 056_powerful_digit_sum.py
@@ -0,0 +1,21 @@
+# Finds the values of a,b<100 that maximise the sum of the digits of a^b
+
+import time
+
+def sum_digits(num):
+    return sum([int(x) for x in str(num)])
+
+start = time.clock()
+
+max_sum = 0
+
+for a in range(1,100):
+    for b in range(1,100):
+        sum_ab = sum_digits(a**b)
+        if sum_ab > max_sum:
+            max_sum = sum_ab
+
+end = time.clock()
+
+print max_sum
+print "Time taken: ", end-start, " s"
