0cad93c9ba8b3c73a667cc179ef1f4bce58a0b54
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-04-04 16:16:12 +0100

    Added solution for problem 79

---
+++ 079_passcode_derivation.py
@@ -0,0 +1,37 @@
+# Finds the shortest number that contains the digits of each of the triplets in
+# external_files/079_keylog.txt in order (not necessarily consecutively)
+
+import time
+import itertools
+
+start = time.clock()
+
+with open('external_files/079_keylog.txt') as f:
+    logs = f.read().splitlines()
+
+digits = set()
+password = []
+digits_before = [[0 for x in range(10)] for x in range(10)]
+
+for log in logs:
+    # Tracks digits present in the file
+    for digit in log:
+        digits.add(int(digit))
+    # Tracks the relative ordering of pairs of digits
+    for a,b in itertools.combinations(log,2):
+        digits_before[int(a)][int(b)] = 1
+
+print digits_before
+counts = [sum(x) for x in digits_before]
+
+
+print counts
+for i in range(9,-1,-1):
+    for index,count in enumerate(counts):
+        if count == i and index in digits:
+            password.append(index)
+
+end = time.clock()
+
+print "".join([str(x) for x in password])
+print "Time taken: ", end-start, " s"
