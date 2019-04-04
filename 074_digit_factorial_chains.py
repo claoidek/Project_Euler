f5132024f63d7ca6b4da03568734abc890b7205b
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-04-04 11:29:52 +0100

    Improved solution for problem 74

--- 074_digit_factorial_chains.py
+++ 074_digit_factorial_chains.py
@@ -1,4 +1,7 @@
-# 
+# Takes each number under a million and forms a chain where each term is equal
+# to the sum of the factorials of the preceding terms digits
+# This is done until a term would be repeated
+# This program finds all such chains that have a length of 60
 
 import time
 
@@ -8,27 +11,40 @@ def factorial_sum(num):
         total += factorials[int(digit)]
     return total
 
-def chain_length(num):
-    length = 1
-    prev = {num}
-    num = factorial_sum(num)
-    if num in master:
-        return master[num]
-    first_num = num
-    while num not in prev:
-        prev.add(num)
-        num = factorial_sum(num)
-    master[first_num] = len(prev)
-    return len(prev)
+def chain_length(num,prev):
+    # We use the sorted digits as the key, because the order of the digits
+    # doesn't matter for the sum of factorials
+    digits = tuple(sorted(str(num)))
+    if digits in master:
+        return master[digits]
+
+    next_term = factorial_sum(num)
+    # End of chain
+    if next_term in prev:
+        return 1
+
+    # If we haven't found the length, call recursively with the next number in
+    # the chain
+    prev.append(next_term)
+    length = chain_length(next_term,prev) + 1
+    # We only record lengths of 4 or greater
+    # This is because the last three numbers may be part of the loop
+    # 3 is the cutoff because we are told that there are no loops with length>3
+    if length > 3:
+        master[digits] = length
+    return length
 
 start = time.clock()
 
+# Stores the chain length for each combination of digits
 master = {}
+
+# Precomputed factorials to save time
 factorials = [1,1,2,6,24,120,720,5040,40320,362880]
 
 sixties = 0
-for i in range(1000001):
-    if chain_length(i) == 60:
+for i in range(1000000):
+    if chain_length(i,[i]) == 60:
         sixties += 1
 
 end = time.clock()
