17f59abc190bf1b6b2fa93381139a79deaa10705
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-04-10 15:11:27 +0100

    Added solution for problem 99

---
+++ 099_largest_exponential.py
@@ -0,0 +1,31 @@
+# Finds the base and exponent combination in external_files/099_base_exp.txt
+# that give the largest number
+# We use the fact that if a^b > c^d, then b*log(a) > d*log(c), which is much
+# easier to calculate
+
+import time
+from math import log
+
+start = time.clock()
+
+with open("external_files/099_base_exp.txt") as f:
+    lines = f.readlines()
+
+lines = [x.strip() for x in lines]
+base_exps = []
+for line in lines:
+    base_exps.append(line.split(","))
+
+biggest = 0
+line_number = -1
+
+for index,[base,exp] in enumerate(base_exps):
+    term = float(exp)*log(float(base))
+    if term > biggest:
+        biggest = term
+        line_number = index + 1
+
+end = time.clock()
+
+print line_number
+print "Time taken: ", end-start, " s"
