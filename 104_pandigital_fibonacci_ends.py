107c156220e06802d0ea722677d3de70455cd6e2
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-04-12 16:56:42 +0100

    Added solution for problem 104

---
+++ 104_pandigital_fibonacci_ends.py
@@ -0,0 +1,45 @@
+# Finds the index of the first Fibonacci number whose first 9 digits and last 9
+# digits are both 1-9 pandigital
+
+from time import clock
+
+def is_pandigital(num):
+    if len(num) == len(set(num)) and "0" not in num:
+        return True
+    return False
+
+start = clock()
+
+first = 0
+second = 1
+end_digits = first + second
+index = 2
+phi = (1 + 5**0.5)/2
+
+while end_digits < 10**8:
+    index += 1
+    first = second
+    second = end_digits
+    end_digits = first + second
+
+start_digits = end_digits
+
+while True:
+    if is_pandigital(str(end_digits)):
+        if is_pandigital(str(start_digits)[:9]):
+            break
+    index += 1
+    first = second
+    second = end_digits
+    # Only the first and last 9 digits matter so we track them separately
+    # end_digits tracks only the last 9
+    end_digits = (first + second)%10**9
+    # start_digits tracks the first 15 digits because extra precision is needed
+    start_digits = int(start_digits*phi)
+    if len(str(start_digits)) > 15:
+        start_digits = int(str(start_digits)[:15])
+
+end = clock()
+
+print index
+print "Time taken: ", end-start, " s"
