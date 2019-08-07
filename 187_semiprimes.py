bc78e0d0bc57da0a17c0481fb4c92329e3cb9640
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2019-08-07 16:52:48 +0100

    Added solution for problem 187

---
+++ 187_semiprimes.py
@@ -0,0 +1,30 @@
+# Finds the number of integers < 10^8 that have exactly two prime factors
+
+from time import clock
+
+def prime_sieve(n):
+    sieve = [True] * n
+    for i in range(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
+    return [2] + [i for i in range(3,n,2) if sieve[i]]
+
+start = clock()
+
+limit = 10**8
+
+primes = prime_sieve(limit/2)
+
+total = 0
+
+for prime1 in primes:
+    for prime2 in primes:
+        if prime2 > prime1 or prime1*prime2 >= limit:
+            break
+        total += 1
+
+
+end = clock()
+
+print total
+print "Time taken: ", end-start, " s"
